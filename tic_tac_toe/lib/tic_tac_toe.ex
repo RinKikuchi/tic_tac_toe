be70dcb8d56a72c490c85fa56e2eb0ee49dd248e
tanukiti <french.papu7@gmail.com>
2021-06-30 10:29:43 +0900

    三目並べ

---
+++ tic_tac_toe/lib/tic_tac_toe.ex
@@ -0,0 +1,14 @@
+defmodule TicTacToe do
+  require Logger
+
+  def info(message) do
+    Logger.info(message)
+  end
+
+  def debug(label, term) do
+    str = inspect(term, pretty: true)
+    Logger.debug("#{label}: #{str}")
+  end
+
+
+end
