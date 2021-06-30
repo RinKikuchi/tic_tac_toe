0a576022b30547f89293f42008873a1f9b66890b
satoru74 <srpk.ol70@gmail.com>
2021-06-30 12:06:23 +0900

    yamanaka first

--- tic_tac_toe/lib/tic_tac_toe/grid.ex
+++ tic_tac_toe/lib/tic_tac_toe/grid.ex
@@ -21,10 +21,57 @@ defmodule TicTacToe.Grid do
 
   def valid_move?(_grid, _x, _y), do: false
 
+  def draw?(grid) do
+    grid
+    |> List.flatten()
+    |> Enum.all?(&(&1 in [:o, :x]))
+  end
+
+  def get_winner(grid) do
+    lines = grid ++ transpose(grid) ++ diagonals(grid)
+
+    line =
+      Enum.find(lines, fn line ->
+        line == [:o, :o, :o] or line == [:x, :x, :x]
+      end)
+
+    if line, do: List.first(line), else: nil
+  end
+
+  defp transpose(grid) do
+    grid
+    |> List.zip()
+    |> Enum.map(&Tuple.to_list/1)
+  end
+
+  defp diagonals(grid) do
+    [diagonal(grid), diagonal(Enum.reverse(grid))]
+  end
+
+  defp diagonal(grid) do
+    grid
+    |> Enum.with_index()
+    |> Enum.map(fn {row, index} -> Enum.at(row, index) end)
+  end
+
   defp get_mark_at(grid, x, y) do
     grid
     |> Enum.at(x)
     |> Enum.at(y)
+  end
+
+  def get_blank_cells(grid) do
+    grid
+    |> Enum.with_index()
+    |> Enum.map(fn {cells, x} -> filter_blank_cells(cells, x) end)
+    |> List.flatten()
+  end
+
+  defp filter_blank_cells(cells, x) do
+    cells
+    |> Enum.with_index()
+    |> Enum.filter(fn {cell, _y} -> cell == :b end)
+    |> Enum.map(fn {_cell, y} -> {x, y} end)
   end
 
 end
